1b4ff33bbc5080e695b604d00b0b556c29ca6908
kiran1926 <kiranroge19@gmail.com>
2025-03-04 00:20:52 -0600

    intro-to-express-lab Exercise 3

--- server.js
+++ server.js
@@ -21,3 +21,19 @@ app.get("/roll/:number", (req, res) => {
     res.send(`you rolled a ${req.params.number}`);
 });
 
+// ==========================  Exercise 3. I Want THAT One!  =============================
+
+app.get("/collectibles/:index", (req, res) => {
+    const collectibles = [
+        { name: 'shiny ball', price: 5.95 },
+        { name: 'autographed picture of a dog', price: 10 },
+        { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
+      ];
+      const i = parseInt(req.params.index);
+      if(!isNaN(i) && i >= 0 && i < collectibles.length){
+      res.send(`So, you want the ${collectibles[i].name}? For ${collectibles[i].price}, it can be yours!`);
+      } else {
+        return res.status(400).send("This item is not yet in stock. Check back soon!");
+      }
+});
+
